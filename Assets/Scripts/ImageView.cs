2eb8a155706130d63d64c18ced906c2919e087cf
JuanjoPrimo <juan_bruguera_20.000@hotmail.com>
2024-02-23 12:03:12 -0300

    changed script ImageView

--- Assets/Scripts/ImageView.cs
+++ Assets/Scripts/ImageView.cs
@@ -27,7 +27,8 @@ public class ImageView : MonoBehaviour
         
         
     }
-    
+ 
+
     public void NextImage()
     {
         List<Sprite> imageList = Manager.Instance.imageList; // Tuve que volver a llamarla sino me saltaba como lista vacia (miss)
@@ -35,8 +36,7 @@ public class ImageView : MonoBehaviour
     if (imageList != null && imageList.Count > 0) // Comprobamos que la lista sea valida (este dentro de rango)
     {
         // Incrementar item (sprites)
-        item = (item + 1) % imageList.Count;
-
+        item = (item + 1) % imageList.Count; 
         // Actualizar la imagen mostrada si el numero de items es valido
         if (item >= 0 && item < imageList.Count)
         {
@@ -62,25 +62,21 @@ public class ImageView : MonoBehaviour
     if (imageList != null && imageList.Count > 0) // Comprobamos que la lista sea valida (este dentro de rango)
     {
         // Incrementar item (sprites)
-        if (item >= 1)
-        {
-        item = (item - 1 + imageList.Count) % imageList.Count;
+        item = (item - 1 + imageList.Count) % imageList.Count; 
         // Actualizar la imagen mostrada si el numero de items es valido
-        if(item >= 0 && item <= imageList.Count)
+        if(item >= 0 && item < imageList.Count)
         {
             displayedImage.sprite = imageList[item];
             displayedImage.enabled = true;
-            Debug.Log("item = " + item);
+            Debug.Log("item = " + item) ;
         }
         else 
         {
             Debug.LogError("Item fuera de rango: " + item);
         }
 
-        }
-            else Debug.LogError("Item no Existente");
-        
     }
+    
     else
     {
         Debug.LogError("La lista de imágenes está vacía.");
